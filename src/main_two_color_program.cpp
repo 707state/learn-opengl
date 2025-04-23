98f1153dec900c5030cae4f3b0c3e82032490f8d
Song Guanjie <songguanjie@meituan.com>
2025-04-23 22:27:21 +0800

    完成了第三道练手题目

---
+++ src/main_two_color_program.cpp
@@ -0,0 +1,117 @@
+#include "common/util.h"
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+const char *vertexShaderSource =
+    "#version 330 core\n"
+    "layout (location=0) in vec3 aPos;\n"
+    "void main(){\n"
+    "gl_Position=vec4(aPos.x,aPos.y,aPos.z,1.0);\n"
+    "}\n\0";
+
+const char *fragmentShader1Source =
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "FragColor = vec4(1.0f,0.5f,0.2f,1.0f);\n"
+    "}\n\0";
+const char *fragmentShader2Source =
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "FragColor=vec4(1.0f,1.0f,1.0f,1.0f);\n"
+    "}\n\0";
+constexpr static unsigned int SRC_WIDTH=800,SRC_HEIGHT=600;
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#if __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT,GL_TRUE);
+#endif
+    GLFWwindow *window =
+        glfwCreateWindow(SRC_WIDTH, SRC_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == nullptr) {
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        return -1;
+    }
+    // 顶点着色器
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    // 两个片段着色器
+    unsigned int fragmentShaderOrange = glCreateShader(GL_FRAGMENT_SHADER);
+    unsigned int fragmentShaderYellow = glCreateShader(GL_FRAGMENT_SHADER);
+    // 创建不同颜色的程序
+    unsigned int shaderProgramOrange = glCreateProgram();
+    unsigned int shaderProgramYellow = glCreateProgram();
+    // 编译上面这一大堆shader
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glShaderSource(fragmentShaderOrange, 1, &fragmentShader1Source, NULL);
+    glShaderSource(fragmentShaderYellow, 1, &fragmentShader2Source, NULL);
+    glCompileShader(vertexShader);
+    glCompileShader(fragmentShaderOrange);
+    glCompileShader(fragmentShaderYellow);
+    // 链接
+    glAttachShader(shaderProgramOrange, vertexShader);
+    glAttachShader(shaderProgramOrange, fragmentShaderOrange);
+    glLinkProgram(shaderProgramOrange);
+    glAttachShader(shaderProgramYellow, vertexShader);
+    glAttachShader(shaderProgramYellow, fragmentShaderYellow);
+    glLinkProgram(shaderProgramYellow);
+    // 配置 顶点数据和顶点属性
+    // clang-format off
+    float firstTriangle[] = {
+        -0.9f,-0.5f,0.0f,
+        -0.0f,-0.5f,0.0f,
+        -0.45f,0.5f,0.0f
+    };
+    float secondTriangle[]={
+        0.0f,-0.5f,0.0f,
+        0.9f,-0.5f,0.0f,
+        0.45f,0.5f,0.0f
+    };
+    // clang-format on
+    unsigned int VBOs[2], VAOs[2];
+    glGenVertexArrays(2, VAOs);
+    glGenBuffers(2, VBOs);
+    // 第一个三角形
+    glBindVertexArray(VAOs[0]);
+    glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(firstTriangle), firstTriangle,
+                 GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
+                          (void *)0);
+    glEnableVertexAttribArray(0);
+    // 第二个三角形
+    glBindVertexArray(VAOs[1]);
+    glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(secondTriangle), secondTriangle,
+                 GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void *)0);
+    glEnableVertexAttribArray(0);
+    while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(shaderProgramOrange);
+        glBindVertexArray(VBOs[0]);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glUseProgram(shaderProgramYellow);
+        glBindVertexArray(VBOs[1]);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glDeleteVertexArrays(2, VAOs);
+    glDeleteBuffers(2, VBOs);
+    glDeleteProgram(shaderProgramOrange);
+    glDeleteProgram(shaderProgramYellow);
+    glfwTerminate();
+    return 0;
+}
